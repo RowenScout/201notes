0044194bda52b836b0396496d21d2b6d6f704043
Rowen Scout <rowenscout@gmail.com>
2017-05-31 18:08:32 -0700

    Added note backlog.

---
+++ loops/loops.js
@@ -0,0 +1,32 @@
+'use strict';
+ //for loops
+ //for (start;stop;step){ do stuff };
+//the below code will console log i for reach instance that i is less than ten
+ for var(i=0;i<10;i++){
+   console.log(i)
+ }
+
+//while loops
+//the below loop does the same thing as the code above.
+//while the arguement within the brackets is true, do stuff in curly brackets
+//while()do{};
+
+ var a = 0;
+ while(a<10){
+   a++
+   console.log(a);
+ }
+
+ //while do loops
+ //this loop will always run the command in do{} at least once, regardless if
+ // the arguement in while{} is true or not.
+
+ var e=0;
+ do{console.log(e)}
+ while (e!=0)
+}
+ //do {
+   //runs at least once!
+   //do some stuff
+//while(true);
+//the code will loop while the arguement is true.
